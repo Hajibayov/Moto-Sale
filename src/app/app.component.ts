d7b71566f5215fda2c8d48dd2449cbd25ba144ef
Hajibayov <shacibyov@gmail.com>
2023-05-31 22:42:51 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'form';
+  apply_form: FormGroup;
+  constructor(public formbilder: FormBuilder) {
+    this.apply_form = this.formbilder.group({
+      name: ['',Validators.required],
+      surname: ['',Validators.required],
+      experience: ['',Validators.required]
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MaterialModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
