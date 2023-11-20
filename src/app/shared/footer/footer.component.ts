56b0db43b448a2c6d0833b64c87076297e6991d8
Hajibayov <shacibyov@gmail.com>
2023-11-20 15:07:41 +0400

    footer completed

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { faCcPaypal } from '@fortawesome/free-brands-svg-icons';
+import { faCcMastercard } from '@fortawesome/free-brands-svg-icons';
+import { faCcVisa } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-  faCcPaypal=faCcPaypal
+  faCcPaypal = faCcPaypal;
+  faCcMastercard = faCcMastercard;
+  faCcVisa = faCcVisa;
 }
