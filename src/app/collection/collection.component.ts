9e3861be111e40d7a1652fffaa2593d41c4cb887
Hajibayov <shacibyov@gmail.com>
2023-11-25 20:50:25 +0400

    pagination logic

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,8 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.scss'],
 })
-export class CollectionComponent {}
+export class CollectionComponent implements OnInit {
+  ary = [0, 1, 2, 3, 4, 5, 6, 7];
+  slicedArrays: any[] = [];
+  currentIndex = 0;
+  iterate: Function | undefined;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.iterate = this.iterator(this.ary, 4);
+    this.updateSlicedArrays();
+  }
+
+  iterator(a: any[], n: number) {
+    let current = 0,
+      l = a.length;
+    return function () {
+      const end = current + n;
+      const part = a.slice(current, end);
+      current = end < l ? end : 0;
+      return part;
+    };
+  }
+
+  updateSlicedArrays() {
+    this.slicedArrays = [];
+    const sliced = this.iterate();
+    this.slicedArrays.push(sliced);
+  }
+
+  onNextClick() {
+    if (this.currentIndex + 4 < this.ary.length) {
+      this.currentIndex += 4;
+      this.iterate = this.iterator(this.ary.slice(this.currentIndex), 4);
+    } else {
+      this.currentIndex = 0;
+      this.iterate = this.iterator(this.ary, 4);
+    }
+    this.updateSlicedArrays();
+  }
+
+  onPreviousClick() {
+    if (this.currentIndex >= 4) {
+      this.currentIndex -= 4;
+      this.iterate = this.iterator(this.ary.slice(this.currentIndex), 4);
+    } else {
+      const lastSetIndex = Math.max(
+        this.ary.length - (this.ary.length % 4 || 4),
+        0
+      );
+      this.currentIndex = lastSetIndex;
+      this.iterate = this.iterator(this.ary.slice(this.currentIndex), 4);
+    }
+    this.updateSlicedArrays();
+  }
+}
