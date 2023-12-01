c13816339511ecc7036e5abb6c822f6f4b8a97f7
Hajibayov <shacibyov@gmail.com>
2023-12-01 22:14:21 +0400

    all-blogs page design updated

--- src/app/all-blogs/all-blogs.component.ts
+++ src/app/all-blogs/all-blogs.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Blog } from '../models/blog.model';
 import { HomeService } from '../home-page/home.service';
-
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-all-blogs',
   templateUrl: './all-blogs.component.html',
   styleUrls: ['./all-blogs.component.scss'],
 })
 export class AllBlogsComponent implements OnInit {
+  faPenToSquare = faPenToSquare;
   routeName = 'Blogs';
   id: string = '';
-  blog: any;
+  currentBlog: any;
   blogs: Blog[] = [];
   constructor(
     private route: ActivatedRoute,
@@ -29,7 +30,9 @@ export class AllBlogsComponent implements OnInit {
       if (this.id) {
         this.homeService
           .getBlogById(this.id)
-          .subscribe((res) => ((this.blog = res), console.log(this.blog)));
+          .subscribe(
+            (res) => ((this.currentBlog = res), console.log(this.currentBlog))
+          );
       }
     });
   }
